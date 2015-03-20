6460cc6af8ccfe133d43cc0ac5a3f49078a39e61
ebc96 <bonete.info4@gmail.com>
2015-03-20 19:59:37 +0100

    Cruilla

---
+++ Cruilla/Cruilla.1/Cruilla.1.ino
@@ -0,0 +1,122 @@
+//****** Variables *******************************************************
+
+
+
+//****** Setup ***********************************************************
+void setup (){  
+pinMode (13, OUTPUT);
+pinMode (12, OUTPUT);
+pinMode (11, OUTPUT);
+pinMode (6, OUTPUT);
+pinMode (5, OUTPUT);
+pinMode (4, OUTPUT);
+}
+
+//****** Loop ***********************************************************
+void loop (){
+ 
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+//2
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+delay (1000);
+
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+delay (1000);
+//4
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+delay (1000);
+
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+digitalWrite(4, HIGH);
+delay (1000);
+//6
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11,LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, HIGH);
+digitalWrite(4, LOW);
+delay (1000);
+
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+
+//8
+digitalWrite(13, LOW);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+
+digitalWrite(13, LOW);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+//10
+digitalWrite(13, LOW);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+
+digitalWrite(13, LOW);
+digitalWrite(12, LOW);
+digitalWrite(11, HIGH);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+//12
+digitalWrite(13, LOW);
+digitalWrite(12, HIGH);
+digitalWrite(11, LOW);
+digitalWrite(6, HIGH);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+
+digitalWrite(13, HIGH);
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+digitalWrite(4, LOW);
+delay (1000);
+}
